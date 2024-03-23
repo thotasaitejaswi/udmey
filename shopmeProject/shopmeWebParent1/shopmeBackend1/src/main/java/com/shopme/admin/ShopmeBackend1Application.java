d1bb8b2fb9d655af0d80829f710e3d8554401ee4
Synergyuniversal <synergyuniversal5@gmail.com>
2024-03-23 16:45:30 +0530

    first commit

---
+++ shopmeProject/shopmeWebParent1/shopmeBackend1/src/main/java/com/shopme/admin/ShopmeBackend1Application.java
@@ -0,0 +1,13 @@
+package com.shopme.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmeBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmeBackend1Application.class, args);
+	}
+
+}
---
+++ shopmeProject/shopmeWebParent1/shopmeFrontend1/src/main/java/com/shopme/site/ShopmeFrontend1Application.java
@@ -0,0 +1,13 @@
+package com.shopme.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmeFrontend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmeFrontend1Application.class, args);
+	}
+
+}
